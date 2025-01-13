98637123c055fdf023e5bf0fedda210c62125eab
taeyeon5211 <jty84373786@gmail.com>
2025-01-13 12:05:27 +0900

    day04_01

---
+++ src/day04/CompareOperatorEx.java
@@ -0,0 +1,22 @@
+package day04;
+
+public class CompareOperatorEx {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 10;
+        boolean result1;
+
+        result1 = n1 == n2;
+        System.out.println(result1);
+        result1 = n1 != n2;
+        System.out.println(result1);
+        result1 = n1 <= n2;
+        System.out.println(result1);
+        result1 = n1 >= n2;
+        System.out.println(result1);
+        result1 = n1 < n2;
+        System.out.println(result1);
+        result1 = n1 > n2;
+        System.out.println(result1);
+    }
+}
